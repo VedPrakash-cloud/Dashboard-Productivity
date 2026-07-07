cd5a4ba9447657ffc95ed0e50fd280ad57208bcf
VedPrakash-cloud <rocky007ved@gmail.com>
2026-07-07 08:43:50 +0530

    testing for background change with static image  according to time

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const body = document.body;
 const navbar = document.querySelector(".navbar")
 
 
-const randomPhoto = async ()=>{
-    try{
-        const res = await fetch('https://api.unsplash.com/photos/random',{
-        headers:{Authorization:`Client-ID ${'cD7tUaaG_dpZ5g_zg-7OXnv_W38ItaN2p8Z6-dgmn0s'}`}
-    })
-    if(!res.ok){
-        throw new Error(`HTTP error! Status: ${res.status}`);
-    }
+// const randomPhoto = async ()=>{
+//     try{
+//         const res = await fetch('https://api.unsplash.com/photos/random',{
+//         headers:{Authorization:`Client-ID ${'cD7tUaaG_dpZ5g_zg-7OXnv_W38ItaN2p8Z6-dgmn0s'}`}
+//     })
+//     if(!res.ok){
+//         throw new Error(`HTTP error! Status: ${res.status}`);
+//     }
 
-    const data = await res.json();
-    const result = data.urls.full
-    body.style.backgroundImage =`url(${result})`;
-    body.style.backgroundPosition="center";
-    body.style.backgroundSize = '100% 100%';
-    body.style.backgroundRepeat = 'no-repeat';
-    }
-    catch(err){
-        console.error("Unable to fetch", err);
-    }
-}
+//     const data = await res.json();
+//     const result = data.urls.full
+//     body.style.backgroundImage =`url(${result})`;
+//     body.style.backgroundPosition="center";
+//     body.style.backgroundSize = '100% 100%';
+//     body.style.backgroundRepeat = 'no-repeat';
+//     }
+//     catch(err){
+//         console.error("Unable to fetch", err);
+//     }
+// }
 
 
 
@@ -40,11 +40,23 @@ setInterval(()=>{
   const rightNow = new Date().getHours();
 
   if(rightNow >= "05" && rightNow <= "11"){
-    randomPhoto();    
+    body.style.backgroundImage =`url(/assets/morning.jpg)`;
+    body.style.backgroundPosition="center";
+    body.style.backgroundSize = '100% 100%';
+    body.style.backgroundRepeat = 'no-repeat';
   }else if(rightNow > "11" && rightNow <= "16"){
-    randomPhoto();
+    body.style.backgroundImage =`url(/assets/afternoon.jpg)`;
+    body.style.backgroundPosition="center";
+    body.style.backgroundSize = '100% 100%';
+    body.style.backgroundRepeat = 'no-repeat';
   }else if(rightNow > "16" && rightNow <= "20"){
-    randomPhoto();
+    body.style.backgroundImage =`url(/assets/evening.jpg)`;
+    body.style.backgroundPosition="center";
+    body.style.backgroundSize = '100% 100%';
+    body.style.backgroundRepeat = 'no-repeat';
   }else{
-    randomPhoto();
+    body.style.backgroundImage =`url(/assets/night.jpg)`;
+    body.style.backgroundPosition="center";
+    body.style.backgroundSize = '100% 100%';
+    body.style.backgroundRepeat = 'no-repeat';
   }
